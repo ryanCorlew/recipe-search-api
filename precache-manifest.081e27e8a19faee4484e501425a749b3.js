57a3ac235229dcc728cb604f640f36be4b2fd85f
Ryan Corlew <ryancorlew13@gmail.com>
2020-05-27 15:42:25 -0400

    Updates

---
+++ precache-manifest.081e27e8a19faee4484e501425a749b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bbc0ff501bb372911f9412b1df1d422",
+    "url": "/recipe-search-api/index.html"
+  },
+  {
+    "revision": "8de8aa21286ee13e1f90",
+    "url": "/recipe-search-api/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5ea38d5a70e7b7fc5432",
+    "url": "/recipe-search-api/static/css/main.06faa172.chunk.css"
+  },
+  {
+    "revision": "8de8aa21286ee13e1f90",
+    "url": "/recipe-search-api/static/js/2.410c0c7d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipe-search-api/static/js/2.410c0c7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ea38d5a70e7b7fc5432",
+    "url": "/recipe-search-api/static/js/main.8addf1aa.chunk.js"
+  },
+  {
+    "revision": "8888b8f13b5118623673",
+    "url": "/recipe-search-api/static/js/runtime-main.2417b097.js"
+  }
+]);
